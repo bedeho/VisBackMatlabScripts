1225fb3e5217933d7b5ea91c5aa9c6602158b5bf
Bedeho Mender <bedeho.mender@psy.ox.ac.uk>
2011-04-15 22:14:39 +0200

    seperating into different files bcs. of matlab calling routines

--- plotAfferentSynapses.m
+++ plotAfferentSynapses.m
@@ -19,19 +19,9 @@ function plotAfferentSynapses(filename, region, col, row, depth, sourceRegion, s
     
     % Read header
     [networkDimensions, list, headerSize] = loadWeightFileHeader(fileID);
+    
+    % Get afferent synapse matrix
+    weightBox = afferentSynapsesMatrixForNeuron(fileID, headerSize, list, region, col, row, depth, sourceRegion, sourceDepth);
 
-    % Read file
-    synapses = afferentSynapsesForNeuron(fileID, headerSize, list, region, col, row, depth);
-    afferentSynapseCount = length(synapses);
-    
-    % Weight box
-    sourceRegionDimension = networkDimensions(sourceRegion).dimension;
-    weightBox = zeros(sourceRegionDimension, sourceRegionDimension);
-    
-    for s = 1:afferentSynapseCount,
-        if synapses(s).regionNr == sourceRegion && synapses(s).depth == sourceDepth
-            weightBox(synapses(s).col, synapses(s).row, synapses(s).depth) = synapses(s).weight;
-        end
-    end
-    
+    % Plot
     plot(weightBox);
