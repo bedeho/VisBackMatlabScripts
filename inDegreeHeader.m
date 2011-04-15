d3e7c526a36d80e740950af770125c60bddc951a
Bedeho Mender <bedeho.mender@psy.ox.ac.uk>
2011-04-15 20:46:29 +0200

    seperating into different files bcs. of matlab calling routines

---
+++ inDegreeHeader.m
@@ -0,0 +1,34 @@
+
+% LOAD INDEGREE HEADER
+% Input=========
+% fileID: Id of open file, expects current file position to be at first
+% byte of indegree header
+% networkDimensions: struct array (dimension,depth) of regions (incl. V1)
+% Output========
+% list: struct array (afferentSynapseCount,offsetCount) of neurons
+% bytesRead: bytes read, this is where the file pointer is left
+function [list, bytesRead] = inDegreeHeader(fileID, networkDimensions)
+
+    % Number of regions, NOT counting V1
+    numRegions = length(networkDimensions) - 1;
+    
+    list = cell(numRegions,1); 
+
+    % Build list of afferentSynapse count for all neurons, and
+    % cumulative sum over afferentSynapseLists up to each neuron (count),
+    % this is for file seeking
+    offsetCount = 0;
+    for r=1:numRegions,
+        list{r} = zeros(networkDimensions(r).dimension, networkDimensions(r).dimension, networkDimensions(r).depth);
+        for d=1:networkDimensions(r).depth, % Region depth
+            for i=1:networkDimensions(r).dimension, % Region row
+                for j=1:networkDimensions(r).dimension, % Region col
+                    afferentSynapseCount = fread(fileID, 1, SOURCE_PLATFORM_USHORT);
+                    list{r}(j,i,d) = struct('afferentSynapseCount', afferentSynapseCount, 'offsetCount' , offsetCount);
+                    offsetCount = offsetCount + afferentSynapseCount;
+                end
+            end
+        end
+    end
+    
+    bytesRead = offsetCount * SYNAPSE_ELEMENT_SIZE;
---
+++ loadWeightFileHeader.m
@@ -0,0 +1,28 @@
+
+% LOAD HEADER OF WEIGHT FILE
+% Input=========
+% fileID: Id of open file
+% Output========
+% networkDimensions: struct array (dimension,depth) of regions (incl. V1)
+% list: struct array (afferentSynapseCount,offsetCount) of neurons
+% bytesRead: bytes read, this is where the file pointer is left
+function [networkDimensions, list, bytesRead] = loadWeightFileHeader(fileID)
+
+    % Seek to start of file
+    fseek(fileID, 0, 'bof');
+    
+    % Read number of regions
+    numRegions = fread(fileID, 1, SOURCE_PLATFORM_USHORT);
+
+    % Preallocate struct array
+    networkDimensions(numRegions).dimension = [];
+    networkDimensions(numRegions).depth = [];
+    
+    % Read dimensions
+    for r=1:numRegions,
+        networkDimensions(r).dimension = fread(fileID, 1, SOURCE_PLATFORM_USHORT);
+        networkDimensions(r).depth = fread(fileID, 1, SOURCE_PLATFORM_USHORT);
+    end
+    
+    [list, bytesRead] = inDegreeHeader(fileID, networkDimensions);
+    bytesRead = bytesRead + SOURCE_PLATFORM_USHORT(1 + 2 * numRegions);
