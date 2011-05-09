272ecdaa23dcc8b6f9e616777dce746cdbca5de7
Bedeho Mender <bedeho.mender@psy.ox.ac.uk>
2011-05-09 17:12:28 +0100

    core feature plotter method built, jesus cristgit push

--- plotFeatureHistory.m
+++ plotFeatureHistory.m
@@ -35,8 +35,9 @@ function plotFeatureHistory(folder, region, depth, row, col, maxEpoch)
     streamSize = maxEpoch * historyDimensions.epochSize;
     
     % Get history array
-    sources = findV1Sources(region, depth, row, col);
+    sources = findV1Sources(networkDimensions, historyDimensions, neuronOffsets, region, depth, row, col, maxEpoch)
     
+    %{
     figure();
     for s=1:length(synapses),
 
@@ -47,26 +48,45 @@ function plotFeatureHistory(folder, region, depth, row, col, maxEpoch)
     end
     
     fclose(fileID);
+    %}
     
-    % sources = struct array (timestep, transform, object, epoch).(col,row,depth, productWeight)
-    
-    function [sources] = findV1Sources(networkDimensions, historyDimensions, neuronOffsets, region, depth, row, col, maxEpoch)
-        
+% sources = cell array {timestep, transform, object, epoch}.(col,row,depth, productWeight)  
+function [sources] = findV1Sources(networkDimensions, historyDimensions, neuronOffsets, region, depth, row, col, maxEpoch)
+
+        THRESHOLD = 0.2;
+
+        % Allocate space for history
+        sources = cell(historyDimensions.numOutputsPrTransform, historyDimensions.numTransforms, historyDimensions.numObjects, maxEpoch);
+            
         if region == 1, % termination condition, V1 cells return them self
-            sources = zeros(historyDimensions.numOutputsPrTransform, historyDimensions.numTransforms, historyDimensions.numObjects, maxEpoch);
-        elseif region > 1, 
+            
+            % Iterate history,
+            for e=1:maxEpoch,
+                for o=1:historyDimensions.numObjects,
+                    for t=1:historyDimensions.numTransforms,
+                        for ti=1:historyDimensions.numOutputsPrTransform,
+                            
+                            % Make 1x1 struct array
+                            v(1).region = region;
+                            v(2).row = row;
+                            v(3).col = col;
+                            v(4).compound = 1;
+                            
+                            sources{ti, t, o, e} = v;
+                        end
+                    end
+                end
+            end
+      elseif region > 1, 
         
             synapses = synapseHistory(fileID, networkDimensions, historyDimensions, neuronOffsets, region, depth, row, col, maxEpoch);
-            % (timestep, transform, object, epoch)
+            afferentSynapseCount = length(synapses);
             
-            for s=1:length(synapses), % For each synapse
+            childSources = cell(afferentSynapseCount, 1);
+            
+            for s=1:afferentSynapseCount % For each child
                 childSources(s) = findV1Sources(networkDimensions, historyDimensions, neuronOffsets, synapses(s).region, synapses(s).depth, synapses(s).row, synapses(s).col, maxEpoch);
             end
-            
-            % Allocate space for history
-            sources(historyDimensions.numOutputsPrTransform, historyDimensions.numTransforms, historyDimensions.numObjects, maxEpoch, length(synapses)).row = [];
-            sources(historyDimensions.numOutputsPrTransform, historyDimensions.numTransforms, historyDimensions.numObjects, maxEpoch, length(synapses)).col = [];
-            sources(historyDimensions.numOutputsPrTransform, historyDimensions.numTransforms, historyDimensions.numObjects, maxEpoch, length(synapses)).region = [];
             
             % Iterate history,
             for e=1:maxEpoch,
@@ -80,24 +100,52 @@ function plotFeatureHistory(folder, region, depth, row, col, maxEpoch)
                             % given certain critirea, e.g. if synapse is
                             % strong enough, or compund weight is strong
                             % enough etc.
-                            for s=1:length(synapses), 
+                            
+                            % Count the number of neurons we will have
+                            childSourcesSize = 0;
+                            for s=1:afferentSynapseCount, 
                                 
+                                if synapses(s).activity(ti, t, o, e) > THRESHOLD,
+                                    childSourcesSize = childSourcesSize + length(childSources{s}{ti, t, o, e});
+                                end
                             end
-
-                            %if synapses(s).activity(ti, t, o, e)
-
-                            %sources = [sources ];
+                            
+                            % Make childSourcesSizex1 struct array
+                            v(childSourcesSize).region = region;
+                            v(childSourcesSize).row = row;
+                            v(childSourcesSize).col = col;
+                            v(childSourcesSize).compound = 1;
+                            
+                            sources{ti, t, o, e} = v;
+                            
+                            counter = 1;
+                            for s=1:afferentSynapseCount, 
+                                
+                                % If this synapse is stronger then
+                                % threshold at this time, then we
+                                % included properly, otherwise we put 0 in
+                                % its place
+                                if synapses(s).activity(ti, t, o, e) > THRESHOLD,
+                                    
+                                     for cs=1:length(childSources{s}{ti, t, o, e}),
+                                         sources{ti, t, o, e}(counter).region = childSources{s}{ti, t, o, e}(cs).region;
+                                         sources{ti, t, o, e}(counter).col = childSources{s}{ti, t, o, e}(cs).col;
+                                         sources{ti, t, o, e}(counter).row = childSources{s}{ti, t, o, e}(cs).row;
+                                         sources{ti, t, o, e}(counter).compound = childSources{s}{ti, t, o, e}(cs).compound * synapses(s).activity(ti, t, o, e);
+                                         counter = counter + 1;    
+                                     end
+                                end
+                            end
                         end
                     end
                 end
             end
-            
         end
     
-    function initCanvas()
+function initCanvas()
     
-    function drawFeature(depth, row, col, weight)
+function drawFeature(depth, row, col, weight)
         
-    function [orrientation, wavelength, phase] = decodeDepth(depth)
+function [orrientation, wavelength, phase] = decodeDepth(depth)
         
     
